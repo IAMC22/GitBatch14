5940366eda44c78e2be3beae2a6ad96e0523fd91
ivanmaravilla <Ivanmaravilla16@gmail.com>
2022-09-27 20:07:01 -0400

    third commit

--- src/class03B14/class03.java
+++ src/class03B14/class03.java
@@ -6,6 +6,7 @@ public class class03 {
 		
 		System.out.println("Hey this is git class");
 		System.out.println("This is a change in my project");
+		System.out.println("Hey how are you liking change?");
 		
 		
 		
